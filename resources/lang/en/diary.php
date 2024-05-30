ea6224ac73bd611c2bd2fbdddd2f100b7aa527e7
eloy <eloylagarjaime@gmail.com>
2024-05-30 15:48:50 +0200

    Control fehca máxima diario

---
+++ resources/lang/en/diary.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'singular' => 'Diary',
+    'plural' => 'Diaries',
+    'form_help' => 'Select a date to display a diary page',
+];
--- resources/views/diary/diaryForm.blade.php
+++ resources/views/diary/diaryForm.blade.php
@@ -5,18 +5,18 @@
 @endsection
 @section('content')
     <div class="wrapper d-flex flex-column">
-        <div class="container col-md-8">
+        <div class="container col-md-4">
             <div class="card">
                 <div class="card-header">
-                    {{ __('evaluations.form_help') }}
+                    {{ __('diary.form_help') }}
                 </div>
                 <div class="card-body">
                     <form action="#" method="POST">
                         @csrf
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
+                        <div class="form-row d-flex">
+                            <div class="form-group col-md-12">
                                 <label for="date-input">{{ __('diary.date') }}</label>
-                                <input type="date" id="date-input" name="date" class="form-control" required>
+                                <input type="date" id="date-input" max="{{ date('Y-m-d') }}" name="date" class="form-control" required>
                             </div>
                         </div>
                         <button type="submit" class="btn float-right">{{ __('crud.submit') }}</button>
