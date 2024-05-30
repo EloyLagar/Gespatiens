45167a10c677020048cb2c5d4b2da0ef3787385b
eloy <eloylagarjaime@gmail.com>
2024-05-30 10:35:08 +0200

    control de errores

--- resources/views/patients/edit.blade.php
+++ resources/views/patients/edit.blade.php
@@ -68,6 +68,17 @@
                             {{ session('success') }}
                         </div>
                         @endif
+                        @if($errors->any())
+                        <div class="alert alert-danger">
+                            {{ $errors->first() }}
+                        </div>
+                        @endif
+                        @if(session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                        @endif
+
                         @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
                         @endforeach
