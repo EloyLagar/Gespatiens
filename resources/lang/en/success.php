f0ec53d2fccae787374aebbec78c371a1fa92f35
Eloy Lagar <elolagjai@alu.edu.gva.es>
2024-06-01 12:45:00 +0200

    mas traducciones de success

---
+++ resources/lang/en/success.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'saved' => 'Successfully saved',
+    'deleted' => 'Successfully deleted',
+    'unsuscribed' => 'Successfully unsuscribed',
+    'updated' => 'Successfully updated',
+    'registered' => 'Successfully registered',
+];
