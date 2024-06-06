b588873f9a466d659c4dc4987fe95e44f4f7618e
eloy <eloylagarjaime@gmail.com>
2024-06-06 12:36:25 +0200

    Traducciones

---
+++ resources/lang/es/activities.php
@@ -0,0 +1,32 @@
+<?php
+return [
+    'singular' => 'Actividad',
+    'plural' => 'Actividades',
+    'date'=> 'Fecha',
+    'type' => 'Tipo',
+    'group' => 'Grupo',
+    'satisfaction' => 'Satisfacción',
+    'utility' => 'Utilidad',
+    'assistants' => 'Asistentes',
+    'attendance' => 'Asistencia',
+    'reducted' => 'Reducción',
+    'justified' => 'Justificado',
+    'no_justified' => 'No justificado',
+    'labels' => [
+        'sport'=>'Deporte',
+        'walk'=>'Paseo',
+        'ther_groups'=>'Gr. terapéuticos',
+        'therapeutic_group'=>'Grupos terapéuticos',
+        'lesson'=>'Lección',
+
+    ],
+    'lesson_type' => [
+        'label' => 'Tipo de lección',
+        'life_skills' => 'Habilidades para la vida',
+        'health_education' => 'Escuela de salud',
+        'carrer_help' => 'Orientación y formación laboral',
+        'occupational_workshop' => 'Taller ocupacional',
+        'video_forum' => 'Vídeo Forum',
+        'maintenance' => 'Mantenimiento',
+    ],
+];
---
+++ resources/lang/es/diary.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+    'singular' => 'Diario',
+    'plural' => 'Diarios',
+    'date' => 'Fecha',
+    'form_help' => 'Selecciona una fecha para mostrar una página del diario',
+    'page' => 'Página',
+    'pages' => 'Páginas',
+    'diary_page' => 'Página del diario',
+
+    'no_activities' => 'Sin actividades',
+    'activities' => [
+        'label' => 'Actividades',
+        'type' => 'Tipo',
+        'assistants' => 'Asistentes',
+        'reducted' => 'Reducción',
+        'justified' => 'Justificado',
+        'no_justified' => 'No justificado',
+        'attends' => 'Asiste',
+    ],
+
+    'intervention' => 'Intervención',
+    'interventions' => 'Intervenciones',
+    'no_interventions' => 'No hay intervenciones',
+    'tutored' => 'Usuarios tutelados',
+
+    'educators' => 'Educadores',
+    'shift' => 'Turno',
+    'shifts' => 'Turnos',
+    'interesting_info' => 'Información interesante',
+    'dayparts' => [
+        'morning' => 'Mañana',
+        'afternoon' => 'Tarde',
+        'night' => 'Noche',
+    ],
+
+    'no_reductions' => 'No hay rebajas',
+    'reductions' => 'Rebajas',
+    'reduction' => [
+        'label' => 'Rebaja',
+        'total' => 'Rebaja TOTAL',
+        'partial' => 'Rebaja parcial (puede hacer...)',
+        'sport' => 'Rebajado en deporte',
+    ],
+
+    'outings_and_arrives' => 'Salidas y llegadas terapéuticas',
+    'outing_or_arrive' => 'Salida o llegada',
+    'outing_or_arrive_time' => 'Hora de salida o llegada',
+    'return_time' => 'Fecha y hora de llegada',
+    'outing_time' => 'Fecha y hora de salida',
+    'outing' => [
+        'exit_date' => 'Fecha y hora de salida',
+        'return_date' => 'Fecha y hora de llegada',
+    ],
+];
+
