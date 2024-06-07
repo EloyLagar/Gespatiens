70cba56f7197ee7c9a69e1b09d80f76a027bf23e
Eloy Lagar <elolagjai@alu.edu.gva.es>
2024-06-07 18:46:25 +0200

    Tabualciones

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <div class="wrapper  d-flex flex-column">
         <a href="{{ route('users.index') }}" class="goBackBtn btn mt-2 mr-auto float-left"><i
-            class='bx bx-left-arrow-alt'></i></a>
+                class='bx bx-left-arrow-alt'></i></a>
         <div class="container col-md-6">
             <div class="card">
                 <div class="card-header">
@@ -21,12 +21,12 @@
                         @method('POST')
 
                         <div class="form-group ">
-                            <label for="name">{{__('user.name')}}:</label>
+                            <label for="name">{{ __('user.name') }}:</label>
                             <input type="text" name="name" class="form-control" id="name"
                                 value="{{ old('name') }}">
                         </div>
                         <div class="form-group ">
-                            <label for="email">{{__('user.email')}}:</label>
+                            <label for="email">{{ __('user.email') }}:</label>
                             <input type="text" name="email" class="form-control" id="email"
                                 value="{{ old('email') }}">
                         </div>
@@ -34,7 +34,7 @@
 
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="specialty">{{__('user.speciality.label')}}:</label>
+                                <label for="specialty">{{ __('user.speciality.label') }}:</label>
                                 <select name="speciality" id="specialty" class="form-control">
                                     @foreach ($enum as $speciality)
                                         <option value="{{ $speciality }}">
@@ -44,7 +44,7 @@
                                 </select>
                             </div>
                             <div class="form-group col-md-6">
-                                <label for="phone">{{__('user.phone')}}:</label>
+                                <label for="phone">{{ __('user.phone') }}:</label>
                                 <input type="text" name="phone_number" class="form-control" id="phone"
                                     value="{{ old('phone_number') }}">
                             </div>
@@ -54,7 +54,7 @@
                             @if ($errors->any())
                                 <ul>
                                     @foreach ($errors->all() as $error)
-                                        <li>{{ __('error.'.$error) }}</li>
+                                        <li>{{ __('error.' . $error) }}</li>
                                     @endforeach
                                 </ul>
                             @endif
